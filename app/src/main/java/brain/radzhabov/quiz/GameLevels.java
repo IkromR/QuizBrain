9628b6bdf16d4a274aac0f78baa362a66c7b8654
Your Name <you@example.com>
2021-04-26 21:50:44 +0300

    Initial commit

---
+++ app/src/main/java/brain/radzhabov/quiz/GameLevels.java
@@ -0,0 +1,22 @@
+package brain.radzhabov.quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
