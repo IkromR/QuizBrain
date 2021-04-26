03afa105e6d6469c04c0ee7cb1b5c10658214a30
Your Name <you@example.com>
2021-04-26 23:43:03 +0300

    normal state application

--- app/src/main/java/brain/radzhabov/quiz/GameLevels.java
+++ app/src/main/java/brain/radzhabov/quiz/GameLevels.java
@@ -1,12 +1,9 @@
 package brain.radzhabov.quiz;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameLevels extends AppCompatActivity {
