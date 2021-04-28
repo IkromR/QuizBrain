8c6ee510f49bf772f1210e192fdbdd7dadf3547b
Your Name <you@example.com>
2021-04-28 21:52:49 +0300

    first level in my game application (forth part)

--- app/src/main/java/brain/radzhabov/quiz/Level1.java
+++ app/src/main/java/brain/radzhabov/quiz/Level1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        //код для скругление углов левой картинки
+        img_left.setClipToOutline(true);
+
+        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        //код для скругление углов правой картинки
+        img_right.setClipToOutline(true);
 
         // Приложение на весь экран - начало
         Window w = getWindow();
