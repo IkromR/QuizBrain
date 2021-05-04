cea9f8f6dc42778af4aa0d90e1b59cd4025e24eb
Your Name <you@example.com>
2021-05-05 01:32:21 +0300

    Java arrays

---
+++ app/src/main/java/brain/radzhabov/quiz/Array.java
@@ -0,0 +1,35 @@
+package brain.radzhabov.quiz;
+
+public class Array {
+    //Массив для первого уровня - начало
+    final int[] image1 = {
+            R.drawable.onelevel_zero,
+            R.drawable.onelevel_one,
+            R.drawable.onelevel_two,
+            R.drawable.onelevel_three,
+            R.drawable.onelevel_four,
+            R.drawable.onelevel_fifth,
+            R.drawable.onelevel_six,
+            R.drawable.onelevel_seven,
+            R.drawable.onelevel_eight,
+            R.drawable.onelevel_nine
+    };
+
+    final int[] power1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    final int[] texts1 = {
+            R.string.lvl1txt0,
+            R.string.lvl1txt1,
+            R.string.lvl1txt2,
+            R.string.lvl1txt3,
+            R.string.lvl1txt4,
+            R.string.lvl1txt5,
+            R.string.lvl1txt6,
+            R.string.lvl1txt7,
+            R.string.lvl1txt8,
+            R.string.lvl1txt9,
+    };
+
+    //Массив для первого уровня - конец
+
+}
